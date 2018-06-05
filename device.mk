b43dd01b8f67a0d2475bf8b4f1960e44c4e995b4
Sagar Rokade <sagarrokade006@gmail.com>
2018-06-05 17:39:35 +0000

    Prebuilts: Retro Music Player,MiXplorer

--- device.mk
+++ device.mk
@@ -43,4 +43,14 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+$(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+#MiXplorer
+PRODUCT_PACKAGES += \
+   MiXplorer
+
+#Music Player
+PRODUCT_PACKAGES += \
+  RetroMusic
+
+
