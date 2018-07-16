45faf1b9703f4faeb5c1f5222aa5e8910a10bdd9
Dixzz <vishusatyam36@gmail.com>
2018-07-16 11:32:25 +0000

    Revert "Add XiaomiParts"
    This reverts commit d30042ecda46f844e55bfa21af529553be0d4572.

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
-# Doze mode
- PRODUCT_PACKAGES += \
-    XiaomiParts \
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.0-service.xiaomi_mido
